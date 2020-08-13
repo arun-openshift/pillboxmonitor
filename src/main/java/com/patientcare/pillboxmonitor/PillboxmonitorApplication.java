58ef2cbfa6e22e10a88dd7aa40cbeecd59df10d4
Kulanthaivel <539617@cognizant.com>
2020-08-13 07:53:40 +0530

    added rest endpoint

--- src/main/java/com/patientcare/pillboxmonitor/PillboxmonitorApplication.java
+++ src/main/java/com/patientcare/pillboxmonitor/PillboxmonitorApplication.java
@@ -5,10 +5,15 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.patientcare.pillboxmonitor.service.MonitorService;
 
 @SpringBootApplication
+@RestController
 public class PillboxmonitorApplication implements CommandLineRunner {
 	
 	@Autowired
@@ -32,6 +37,11 @@ public class PillboxmonitorApplication implements CommandLineRunner {
 //		monitorService.publish(topic, "Welcome openshift1", 0, true);
 
 //		context.close();
+	}
+	
+	@RequestMapping(value = "/areyouthere")
+	public ResponseEntity<String> getGreeting() {
+		return new ResponseEntity<String>("iMHERE", HttpStatus.OK);
 	}
 
 }
